eb61e028b715ac6868faa6f79bba31943d7f0492
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-04-21 13:20:57 +0200

    Jest ignore dist directory

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   maxWorkers: 2,
   setupFilesAfterEnv: ["./src/setupTests.ts"],
+  testPathIgnorePatterns: [
+    "/dist/",
+    "/node_modules/"
+  ],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
