dd1e404ad9d783e5b1ce5b3ee79f59665e982b9f
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-04-21 13:30:20 +0200

    Serve static assets

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import path from "path";
 
 import { PORT } from "./config";
 import { server } from "./server";
 
 const app = express();
 server.applyMiddleware({ app });
+
+const distDir = path.join(__dirname, "../../web/dist");
+app.use(express.static(distDir));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(distDir, "index.html"));
+});
 
 app.listen({ port: PORT }, () => {
   console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
