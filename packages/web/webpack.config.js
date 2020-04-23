6a4bec4141a68718b5466a67277358d28d78b759
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-04-23 10:56:12 +0200

    Improve compilation speed

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -40,7 +40,12 @@ module.exports = {
       },
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: [
+          {
+            loader: "ts-loader",
+            options: { transpileOnly: true }
+          }
+        ],
         exclude: "/node_modules/"
       }
     ],
