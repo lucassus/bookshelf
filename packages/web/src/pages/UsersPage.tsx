2d3285d09163f7a54dd918c5fcacb6ec018bebc6
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-04-23 12:56:29 +0200

    Feature: Users page (#22)
    * Tune webpack dev server config
    
    * Add simple users page
    
    * Make the linter happy

--- packages/web/src/pages/UsersPage.tsx
+++ packages/web/src/pages/UsersPage.tsx
@@ -1,10 +1,67 @@
-import { Container, Typography } from "@material-ui/core";
+import { gql, useQuery } from "@apollo/client";
+import {
+  CircularProgress,
+  Container,
+  Grid,
+  Typography
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import React from "react";
 
-export const UsersPage: React.FunctionComponent = () => (
-  <Container>
-    <Typography variant="h4" component="h2">
-      Users
-    </Typography>
-  </Container>
-);
+import { UserAvatar } from "../components/UserAvatar";
+import { User } from "../types";
+
+export const USERS_QUERY = gql`
+  query {
+    users {
+      name
+      avatar {
+        image {
+          url
+        }
+        color
+      }
+    }
+  }
+`;
+
+export const UsersPage: React.FunctionComponent = () => {
+  const { loading, error, data } = useQuery<{
+    users: User[];
+  }>(USERS_QUERY);
+
+  if (loading) {
+    return (
+      <div>
+        <CircularProgress />
+        <span>Loading users...</span>
+      </div>
+    );
+  }
+
+  if (error || !data) {
+    return <Alert severity="error">Could not load users...</Alert>;
+  }
+
+  return (
+    <Container>
+      <Typography variant="h4" component="h2">
+        Users
+      </Typography>
+
+      <Grid
+        container
+        direction="row"
+        justify="space-around"
+        alignItems="center"
+        spacing={3}
+      >
+        {data.users.map((user) => (
+          <Grid item key={user.email}>
+            <UserAvatar user={user} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
--- packages/web/src/types.ts
+++ packages/web/src/types.ts
@@ -6,3 +6,12 @@ export interface Author {
   name: string;
   photo: Image;
 }
+
+export interface User {
+  name: string;
+  email: string;
+  avatar: {
+    image: Image;
+    color: string;
+  };
+}
