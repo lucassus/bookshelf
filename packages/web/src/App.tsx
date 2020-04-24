dc4d13e89d2a07345ede199d0d37319784cb1dc0
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-04-24 08:58:27 +0200

    Create app top bar component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,41 +1,16 @@
-import {
-  Typography,
-  Container,
-  CssBaseline,
-  AppBar,
-  Toolbar,
-  Button
-} from "@material-ui/core";
+import { Container, CssBaseline } from "@material-ui/core";
 import React from "react";
-import { Routes, Route, Link as RouterLink } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { AuthorsPage } from "./pages/AuthorsPage";
 import { UsersPage } from "./pages/UsersPage";
 import { BooksPage } from "./pages/BooksPage";
+import { AppTopBar } from "./components/AppTopBar";
 
 export const App: React.FunctionComponent = () => (
   <>
     <CssBaseline />
-
-    <AppBar position="relative">
-      <Toolbar>
-        <Typography variant="h6" component="h1">
-          Personal Library
-        </Typography>
-
-        <nav>
-          <Button component={RouterLink} to="/">
-            Books
-          </Button>
-          <Button component={RouterLink} to="/authors">
-            Authors
-          </Button>
-          <Button component={RouterLink} to="/users">
-            Users
-          </Button>
-        </nav>
-      </Toolbar>
-    </AppBar>
+    <AppTopBar />
 
     <Container maxWidth="lg">
       <Routes>
