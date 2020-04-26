458212efc156de11bb73c572bc3ba8a2412d2337
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-04-26 16:33:55 +0200

    Remove migrations for now

--- packages/server/ormconfig.js
+++ packages/server/ormconfig.js
@@ -8,11 +8,7 @@ module.exports = {
   synchronize: true,
   logging: true,
   entities: ["src/entity/**/*.ts"],
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscriber/**/*.ts"],
   cli: {
-    entitiesDir: "src/entity",
-    migrationsDir: "src/migration",
-    subscribersDir: "src/subscriber"
+    entitiesDir: "src/entity"
   }
 };
