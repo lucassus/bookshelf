19846f304290a6ff7867bb2be62726d615664a5a
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-04-30 20:48:40 +0200

    Add a storybook for AuthorCard

---
+++ packages/web/src/components/AuthorCard.stories.tsx
@@ -0,0 +1,30 @@
+import { text, withKnobs } from "@storybook/addon-knobs";
+import React from "react";
+import { MemoryRouter } from "react-router";
+
+import { Author } from "../types.generated";
+import { AuthorCard } from "./AuthorCard";
+
+export default {
+  title: "AuthorCard",
+  component: AuthorCard,
+  decorators: [withKnobs]
+};
+
+export const simple = () => {
+  const author: Author = {
+    id: 1,
+    name: text("Author Name", "J. K. Rowling"),
+    photo: {
+      url:
+        "http://examples.devmastery.pl/assets/images/book-authors/j-k-rowling.jpg"
+    },
+    books: []
+  };
+
+  return (
+    <MemoryRouter>
+      <AuthorCard author={author} />
+    </MemoryRouter>
+  );
+};
