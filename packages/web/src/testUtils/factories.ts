e9f8bc86477f75c7e2196f4d7c9a576e63e34622
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-05-05 09:57:15 +0200

    Fix test factories

--- packages/web/src/testUtils/factories.ts
+++ packages/web/src/testUtils/factories.ts
@@ -5,6 +5,7 @@ type Factory<T> = (attributes?: Partial<T>) => T;
 export const createAuthor: Factory<Author> = (attributes = {}) => ({
   id: 1,
   name: "J. K. Rowling",
+  bio: "As a child, Rowling often wrote fantasy stories",
   photo: {
     url:
       "http://examples.devmastery.pl/assets/images/book-authors/j-k-rowling.jpg"
@@ -15,6 +16,7 @@ export const createAuthor: Factory<Author> = (attributes = {}) => ({
 export const createBook: Factory<Book> = (attributes = {}) => ({
   id: 1,
   title: "Test book",
+  description: "Lorem impsum",
   favourite: false,
   cover: {
     url: "http://examples.devmastery.pl/assets/images/book-covers/witcher1.jpg"
@@ -25,6 +27,7 @@ export const createBook: Factory<Book> = (attributes = {}) => ({
 export const createUser: Factory<User> = (attributes = {}) => ({
   id: 1,
   name: "Bob",
+  info: "Professional problem solver.",
   email: "bob@email.com",
   avatar: {
     image: {
