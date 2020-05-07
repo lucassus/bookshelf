c4bfe31e20ee5166e763f16f9801c2fbb00ca42f
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-05-07 14:46:15 +0200

    Fix babel config path for monorepos

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const package = require("./package.json");
 
 module.exports = {
@@ -7,5 +9,11 @@ module.exports = {
   },
   maxWorkers: 1,
   setupFilesAfterEnv: ["./src/setupTests.ts"],
-  testPathIgnorePatterns: ["/dist/", "/node_modules/"]
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "babel-jest",
+      { configFile: path.join(__dirname, "babel.config.js") }
+    ]
+  }
 };
