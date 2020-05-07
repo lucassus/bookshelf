f047fbe87fdc4e2ee294d1669f5353065e6def92
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-05-07 13:21:30 +0200

    Migrate from ts-jest to babel

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -5,37 +5,6 @@ module.exports = {
     color: "green",
     name: package.name
   },
-  preset: "ts-jest",
-  globals: {
-    "ts-jest": {
-      babelConfig: {
-        // See https://material-ui.com/guides/minimizing-bundle-size/
-        plugins: [
-          [
-            "babel-plugin-import",
-            {
-              libraryName: "@material-ui/core",
-              // Use "'libraryDirectory': ''," if your bundler does not support ES modules
-              libraryDirectory: "esm",
-              camel2DashComponentName: false
-            },
-            "core"
-          ],
-          [
-            "babel-plugin-import",
-            {
-              libraryName: "@material-ui/icons",
-              // Use "'libraryDirectory': ''," if your bundler does not support ES modules
-              libraryDirectory: "esm",
-              camel2DashComponentName: false
-            },
-            "icons"
-          ]
-        ]
-      }
-    }
-  },
-  testEnvironment: "node",
   maxWorkers: 1,
   setupFilesAfterEnv: ["./src/setupTests.ts"],
   testPathIgnorePatterns: ["/dist/", "/node_modules/"]
