4cd48df9f0120710614fbc49e01a07b74b0125e0
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-05-07 13:32:12 +0200

    Setup babel-plugin-import

--- packages/web/babel.config.js
+++ packages/web/babel.config.js
@@ -3,5 +3,25 @@ module.exports = {
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-react",
     "@babel/preset-typescript"
+  ],
+  plugins: [
+    [
+      "import",
+      {
+        libraryName: "@material-ui/core",
+        libraryDirectory: "",
+        camel2DashComponentName: false
+      },
+      "core"
+    ],
+    [
+      "import",
+      {
+        libraryName: "@material-ui/icons",
+        libraryDirectory: "",
+        camel2DashComponentName: false
+      },
+      "icons"
+    ]
   ]
 };
