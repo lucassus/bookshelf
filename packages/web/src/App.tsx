3b0f5a8e342e652c02c91d232ef587256447580e
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-06-18 21:42:28 +0200

    Refactor some names

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -4,9 +4,9 @@ import { Route, Routes } from "react-router-dom";
 import { AppTopBar } from "./components/AppTopBar";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import { AuthorDetailsPage } from "./pages/AuthorDetailsPage";
-import { AuthorsPage } from "./pages/AuthorsPage/AuthorsPage";
+import { AuthorsPage } from "./pages/AuthorsPage";
 import { BookDetailsPage } from "./pages/BookDetailsPage";
-import { BooksPage } from "./pages/BooksPage/BooksPage";
+import { BooksPage } from "./pages/BooksPage";
 import { UserDetailsPage } from "./pages/UserDetailsPage";
 import { UsersPage } from "./pages/UsersPage";
 
---
+++ packages/web/src/components/AppTopBar/index.ts
@@ -0,0 +1,1 @@
+export { AppTopBar } from "./AppTopBar";
