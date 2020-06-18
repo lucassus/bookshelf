a8ede021920a56ba34693e8fea0bc54bf1400759
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-06-18 13:35:26 +0200

    Install sass

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -30,10 +30,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.(css|scss)$/i,
         use: [
           "style-loader",
-          { loader: "css-loader", options: { modules: true } }
+          { loader: "css-loader", options: { modules: true } },
+          "sass-loader"
         ]
       },
       {
