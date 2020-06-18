52e7cbd8200b8eaf21590d0e8db9cfe4180713f5
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-06-18 13:14:25 +0200

    Enable css modules

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -31,7 +31,10 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { modules: true } }
+        ]
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
