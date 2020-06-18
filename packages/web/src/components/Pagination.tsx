467cc0cb86ab90bb5b26971ed30ae5c2c4eb63dc
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-06-18 12:22:43 +0200

    Implement a very simple pager

--- packages/web/src/components/Pagination.tsx
+++ packages/web/src/components/Pagination.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 type Props = {
-  onChange: (page: number) => any;
   count: number;
+  onChange: (page: number) => any;
 };
 
-export const Pagination: React.FunctionComponent<Props> = () => (
-  <div>Pagination goes here...</div>
-);
+// TODO: Write test, prettify and refactor
+// TODO: Create a storybook
+export const Pagination: React.FunctionComponent<Props> = ({
+  count,
+  onChange
+}) => {
+  const pages = useMemo(
+    () => new Array(count).fill(null).map((_, index) => index + 1),
+    [count]
+  );
+
+  return (
+    <div>
+      <button onClick={() => onChange(1)}>first</button>
+
+      {pages.map((page) => (
+        <button key={page} onClick={() => onChange(page)}>
+          {page}
+        </button>
+      ))}
+
+      <button onClick={() => onChange(count)}>last</button>
+    </div>
+  );
+};
