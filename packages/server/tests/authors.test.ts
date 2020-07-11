5e32adf087f5b70259dbedaf60fd44d134d3fdaf
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-11 16:42:54 +0200

    Separate integration test for authors

---
+++ packages/server/tests/authors.test.ts
@@ -0,0 +1,62 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import { createTestClient } from "apollo-server-testing";
+import { Connection, getConnection } from "typeorm";
+
+import { secureId } from "../src/database/helpers";
+import { createServer } from "../src/server";
+
+let connection: Connection;
+let server: ApolloServer;
+
+beforeEach(async () => {
+  connection = getConnection();
+  server = createServer(connection);
+});
+
+it("fetches an author", async () => {
+  const { query } = createTestClient(server);
+
+  // When
+  const res = await query({
+    query: gql`
+      query GetAuthor($id: ID!) {
+        author(id: $id) {
+          id
+          name
+          bio
+          books {
+            title
+          }
+        }
+      }
+    `,
+    variables: { id: secureId.toExternal(1, "Author") }
+  });
+
+  // Then
+  expect(res.data).not.toBeUndefined();
+  expect(res.data).toMatchSnapshot();
+});
+
+it("fetches authors along with books", async () => {
+  // Given
+  const { query } = createTestClient(server);
+
+  // When
+  const res = await query({
+    query: gql`
+      query {
+        authors {
+          name
+          books {
+            title
+          }
+        }
+      }
+    `
+  });
+
+  // Then
+  expect(res.data).not.toBeUndefined();
+  expect(res.data).toMatchSnapshot();
+});
--- packages/server/tests/books.test.ts
+++ packages/server/tests/books.test.ts
@@ -1,6 +1,7 @@
 import { ApolloServer, gql } from "apollo-server-express";
 import { createTestClient } from "apollo-server-testing";
 import { Connection, getConnection } from "typeorm";
+import { Author } from "../src/database/entity/Author";
 
 import { Book } from "../src/database/entity/Book";
 import { secureId } from "../src/database/helpers";
@@ -143,4 +144,92 @@ it("responds with error when book cannot be found", async () => {
   expect(res.errors![0].message).toEqual(
     'Could not find any entity of type "Book" matching: "200"'
   );
+});
+
+it("fetches books along with authors and books again", async () => {
+  // Given
+  const { query } = createTestClient(server);
+
+  // When
+  const res = await query({
+    query: gql`
+      query {
+        books {
+          title
+          author {
+            name
+            books {
+              title
+            }
+          }
+        }
+      }
+    `
+  });
+
+  // Then
+  expect(res.data).not.toBeUndefined();
+  expect(res.data).toMatchSnapshot();
+});
+
+it("fetches a random book", async () => {
+  // Given
+  await connection.createQueryBuilder().delete().from(Book).execute();
+  const author = await connection.manager.findOneOrFail(Author, {
+    name: "Andrzej Sapkowski"
+  });
+  await connection.manager.save(Book, {
+    authorId: author.id,
+    title: "The tower of the swallow",
+    coverPath: "/images/book-covers/witcher4.jpg"
+  });
+
+  const { query } = createTestClient(server);
+
+  // When
+  const res = await query({
+    query: gql`
+      query {
+        randomBook {
+          title
+          cover {
+            url
+          }
+        }
+      }
+    `
+  });
+
+  // Then
+  expect(res.data).not.toBeUndefined();
+  expect(res.data!.randomBook).toMatchSnapshot();
+});
+
+it("updates book favourite", async () => {
+  // Given
+  const book = await connection.manager.findOneOrFail(Book, 1);
+  expect(book.favourite).toBe(false);
+
+  const { mutate } = createTestClient(server);
+
+  // When
+  const res = await mutate({
+    mutation: gql`
+      mutation UpdateBookFavourite($id: ID!, $favourite: Boolean) {
+        updateBookFavourite(id: $id, favourite: $favourite) {
+          id
+          title
+          favourite
+        }
+      }
+    `,
+    variables: { id: secureId.toExternal(book.id, "Book"), favourite: true }
+  });
+
+  // Then
+  expect(res.data).not.toBeUndefined();
+  expect(res.data!.updateBookFavourite).toMatchSnapshot();
+
+  const updatedBook = await connection.manager.findOneOrFail(Book, 1);
+  expect(updatedBook.favourite).toBe(true);
 });
