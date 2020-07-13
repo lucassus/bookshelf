293b66b9619d42b87d76fdf9ecdec00bafb1a74c
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-13 18:27:26 +0200

    Minor refactoring add some TODOs

--- packages/server/src/database/authorsLoader.ts
+++ packages/server/src/database/authorsLoader.ts
@@ -3,20 +3,26 @@ import { getConnection } from "typeorm";
 
 import { Author } from "./entity/Author";
 
+// TODO: The Array of values must be the same length as the Array of keys.
+// TODO: Each index in the Array of values must correspond to the same index in the Array of keys.
+const batchLoadAuthors: DataLoader.BatchLoadFn<number, Author> = async (
+  ids
+) => {
+  const authors = await getConnection().manager.findByIds(
+    Author,
+    ids as number[]
+  );
+
+  const byId: Record<number, Author> = authors.reduce(
+    (result, author) => ({
+      ...result,
+      [author.id]: author
+    }),
+    {}
+  );
+
+  return ids.map((authorId) => byId[authorId]);
+};
+
 export const buildAuthorsLoader = () =>
-  new DataLoader<number, Author>(async (keys) => {
-    const authors = await getConnection().manager.findByIds(
-      Author,
-      keys as number[]
-    );
-
-    const byId: Record<number, Author> = authors.reduce(
-      (result, author) => ({
-        ...result,
-        [author.id]: author
-      }),
-      {}
-    );
-
-    return keys.map((authorId) => byId[authorId]);
-  });
+  new DataLoader<number, Author>(batchLoadAuthors);
