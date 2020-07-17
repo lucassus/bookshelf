3cf174e245e065999f7358fa6a5c58b4aeaa6979
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-17 14:08:00 +0200

    Add links to book copy owner and borrower

--- packages/web/src/components/BookCopy/BookCopy.tsx
+++ packages/web/src/components/BookCopy/BookCopy.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import { Avatar } from "../Avatar";
 import { BookCopyFragment } from "./BookCopy.fragment.generated";
@@ -8,22 +9,29 @@ type Props = {
   bookCopy: BookCopyFragment;
 };
 
-// TODO: Display links to users
 export const BookCopy: React.FunctionComponent<Props> = ({ bookCopy }) => (
   <div className={styles.container}>
     <img src={bookCopy.book.cover.url} alt={bookCopy.book.title} />
 
     <div className={styles.ownerAvatar}>
-      <Avatar name={bookCopy.owner.name} small avatar={bookCopy.owner.avatar} />
+      <Link to={`/users/${bookCopy.owner.id}`}>
+        <Avatar
+          name={bookCopy.owner.name}
+          small
+          avatar={bookCopy.owner.avatar}
+        />
+      </Link>
     </div>
 
     {bookCopy.borrower && (
       <div className={styles.borrowerAvatar}>
-        <Avatar
-          name={bookCopy.borrower.name}
-          small
-          avatar={bookCopy.borrower.avatar}
-        />
+        <Link to={`/users/${bookCopy.borrower.id}`}>
+          <Avatar
+            name={bookCopy.borrower.name}
+            small
+            avatar={bookCopy.borrower.avatar}
+          />
+        </Link>
       </div>
     )}
   </div>
