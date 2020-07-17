8fd220e5ee7f34f2e1b0c8d54df791566231a7ba
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-17 16:15:43 +0200

    Minor refactoring for book card mutation

--- packages/web/src/components/BookCard/BookCard.tsx
+++ packages/web/src/components/BookCard/BookCard.tsx
@@ -11,14 +11,13 @@ type Props = {
 };
 
 export const BookCard: React.FunctionComponent<Props> = ({ book }) => {
-  const [updateFavourite] = useUpdateBookFavouriteMutation();
+  const [updateFavourite] = useUpdateBookFavouriteMutation({
+    variables: { id: book.id, favourite: !book.favourite }
+  });
 
   const handleToggleFavourite = (event: MouseEvent) => {
     event.stopPropagation();
-
-    return updateFavourite({
-      variables: { id: book.id, favourite: !book.favourite }
-    });
+    return updateFavourite();
   };
 
   return (
