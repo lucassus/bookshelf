f6df001a1d97bfe6aa5daf537fbf593931ae7b8d
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-18 18:42:48 +0200

    Add simple 404 page

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -9,6 +9,7 @@ import { AuthorDetailsPage } from "./pages/AuthorDetailsPage";
 import { AuthorsPage } from "./pages/AuthorsPage";
 import { BookDetailsPage } from "./pages/BookDetailsPage";
 import { BooksPage } from "./pages/BooksPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
 import { UserDetailsPage } from "./pages/UserDetailsPage";
 import { UsersPage } from "./pages/UsersPage";
 
@@ -24,6 +25,7 @@ export const App: React.FunctionComponent = () => (
         <Route path="/authors/:id" element={<AuthorDetailsPage />} />
         <Route path="/users" element={<UsersPage />} />
         <Route path="/users/:id" element={<UserDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </main>
   </ErrorBoundary>
