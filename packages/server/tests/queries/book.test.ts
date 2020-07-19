efbffe6e5d7b6d9c6e88dba40becd9e186bc6323
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-19 17:12:20 +0200

    An idea for better tests

---
+++ packages/server/tests/queries/book.test.ts
@@ -0,0 +1,119 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import { createTestClient } from "apollo-server-testing";
+
+import {
+  createAuthor,
+  createBook,
+  createBookCopy,
+  createUser
+} from "../../src/database/factories";
+import { secureId } from "../../src/database/helpers";
+import { createServer } from "../../src/server";
+
+let server: ApolloServer;
+
+beforeEach(async () => {
+  server = createServer();
+});
+
+test("book query", async () => {
+  // Given
+  const { query } = createTestClient(server);
+
+  const author = await createAuthor({ name: "Andrzej Sapkpwski" });
+  const book = await createBook({
+    authorId: author.id,
+    title: "Blood of Elves",
+    description: "Blood of Elves is the first novel in the Witcher Saga.",
+    createdAt: new Date(Date.UTC(2020, 6, 19, 14, 30)),
+    updatedAt: new Date(Date.UTC(2020, 6, 19, 14, 45))
+  });
+
+  const owner = await createUser({
+    name: "John",
+    avatarAttributes: { color: "yellow" }
+  });
+  const borrower = await createUser({ name: "Paul" });
+  await createBookCopy({
+    bookId: book.id,
+    ownerId: owner.id,
+    borrowerId: borrower.id
+  });
+  await createBookCopy({ bookId: book.id });
+
+  // When
+  const id = secureId.toExternal(book.id, "Book");
+  const res = await query({
+    query: gql`
+      query GetBook($id: ID!) {
+        book(id: $id) {
+          id
+          title
+          description
+          createdAt
+          updatedAt
+          copies {
+            id
+            owner {
+              id
+              name
+              avatar {
+                image {
+                  path
+                  url
+                }
+                color
+              }
+            }
+            borrower {
+              id
+              name
+              avatar {
+                image {
+                  path
+                  url
+                }
+                color
+              }
+            }
+          }
+        }
+      }
+    `,
+    variables: { id }
+  });
+
+  // Then
+  expect(res.data).not.toBeNull();
+  expect(res.data).toMatchObject({
+    book: {
+      id,
+      title: book.title,
+      description: book.description,
+      copies: [
+        {
+          id: expect.any(String),
+          owner: {
+            id: expect.any(String),
+            name: owner.name,
+            avatar: {
+              image: {
+                path: expect.any(String),
+                url: expect.any(String)
+              },
+              color: "yellow"
+            }
+          },
+          borrower: expect.objectContaining({
+            id: expect.any(String)
+          })
+        },
+        expect.objectContaining({
+          id: expect.any(String)
+        })
+      ],
+      createdAt: "2020-07-19T14:30:00.000Z",
+      updatedAt: "2020-07-19T14:45:00.000Z"
+    }
+  });
+});
