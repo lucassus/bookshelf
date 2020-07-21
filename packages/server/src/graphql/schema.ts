a0bf63b25067a67609d8430329ce946e60f471c4
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-21 11:59:23 +0200

    Use ExternalID in the other places

--- packages/server/src/graphql/schema.ts
+++ packages/server/src/graphql/schema.ts
@@ -5,16 +5,12 @@ import path from "path";
 
 import { secureId } from "../database/helpers";
 
-export const externalIDScalarType = new GraphQLScalarType({
-  name: "ExternalID",
-  // TODO: Implement the serialization
-  // serialize: (value) => value,
-  parseValue: (value) => secureId.toInternal(value)
-});
-
 export const schema = loadSchemaSync(path.join(__dirname, "./**/*.graphql"), {
   loaders: [new GraphQLFileLoader()],
   resolvers: {
-    ExternalID: externalIDScalarType
+    ExternalID: new GraphQLScalarType({
+      name: "ExternalID",
+      parseValue: (value) => secureId.toInternal(value)
+    })
   }
 });
