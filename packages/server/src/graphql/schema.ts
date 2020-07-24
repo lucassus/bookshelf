282e69fc2da56ca936ebc8a7141bcdd7bc933288
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-24 15:20:13 +0200

    Refactor loading resolvers

--- packages/server/src/graphql/schema.ts
+++ packages/server/src/graphql/schema.ts
@@ -1,13 +1,26 @@
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { loadSchemaSync } from "@graphql-tools/load";
+import { mergeResolvers } from "@graphql-tools/merge";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import path from "path";
 
-import { resolvers } from "./resolvers";
+import { Context } from "../types";
+import { Resolvers } from "./resolvers-types.generated";
 
 const schema = loadSchemaSync(path.join(__dirname, "./**/*.graphql"), {
   loaders: [new GraphQLFileLoader()]
 });
+
+const resolvers = mergeResolvers<Context, Resolvers<Context>>([
+  require("./Anything").resolvers,
+  require("./Author").resolvers,
+  require("./Book").resolvers,
+  require("./BookCopy").resolvers,
+  require("./ExternalID").resolvers,
+  require("./Resource").resolvers,
+  require("./Timestampable").resolvers,
+  require("./User").resolvers
+]);
 
 export const schemaWithResolvers = addResolversToSchema({
   schema,
