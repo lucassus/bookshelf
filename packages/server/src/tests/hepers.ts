083fc460aaab3de4cadc0eae6e25fd147bfa0d29
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-25 10:47:04 +0200

    Fix a typo

--- packages/server/src/tests/hepers.ts
+++ packages/server/src/tests/hepers.ts
@@ -8,24 +8,21 @@ import { getConnection } from "typeorm";
 import { ASSETS_BASE_URL } from "../config";
 import { buildAuthorsLoader } from "../database/authorsLoader";
 import { User } from "../database/entity/User";
-import { schemaWithResolvers } from "../graphql/schema";
-import { Context } from "../types";
+import { rootSchema } from "../graphql/schema";
 
 export const getTestClient = ({
   currentUser
 }: {
   currentUser?: User;
 } = {}): ApolloServerTestClient => {
-  const context: Context = {
-    assetsBaseUrl: ASSETS_BASE_URL,
-    authorsLoader: buildAuthorsLoader(),
-    connection: getConnection(),
-    currentUser
-  };
-
   const server = new ApolloServer({
-    schema: schemaWithResolvers,
-    context
+    schema: rootSchema,
+    context: {
+      assetsBaseUrl: ASSETS_BASE_URL,
+      authorsLoader: buildAuthorsLoader(),
+      connection: getConnection(),
+      currentUser
+    }
   });
 
   return createTestClient(server);
