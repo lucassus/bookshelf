77e83d5f4d732737df43533b9f6c3700da7f63a1
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-25 14:24:14 +0200

    Auto-load resolvers

--- packages/server/src/graphql/rootSchema.ts
+++ packages/server/src/graphql/rootSchema.ts
@@ -1,22 +1,16 @@
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { loadSchemaSync } from "@graphql-tools/load";
+import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers } from "@graphql-tools/merge";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import path from "path";
-
-import { Context } from "../common/types";
-import { Resolvers } from "./resolvers-types.generated";
 
 export const rootSchema = addResolversToSchema({
   schema: loadSchemaSync(path.join(__dirname, "./**/schema.graphql"), {
     loaders: [new GraphQLFileLoader()]
   }),
-  resolvers: mergeResolvers<Context, Resolvers<Context>>([
-    require("./common/resolvers"),
-    require("./users/resolvers"),
-    require("./authentication/resolvers"),
-    require("./authors/resolvers"),
-    require("./books/resolvers")
-  ]),
+  resolvers: mergeResolvers(
+    loadFilesSync(path.join(__dirname, "./**/resolvers.{ts,js}"))
+  ),
   inheritResolversFromInterfaces: true
 });
