695542ae60ae764556715fca15c5129dc7194731
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-25 10:12:57 +0200

    Minor refactoring

--- packages/server/src/graphql/schema.ts
+++ packages/server/src/graphql/schema.ts
@@ -7,23 +7,19 @@ import path from "path";
 import { Context } from "../types";
 import { Resolvers } from "./resolvers-types.generated";
 
-const schema = loadSchemaSync(path.join(__dirname, "./**/*.graphql"), {
-  loaders: [new GraphQLFileLoader()]
-});
-
-const resolvers = mergeResolvers<Context, Resolvers<Context>>([
-  require("./Anything").resolvers,
-  require("./Author").resolvers,
-  require("./Book").resolvers,
-  require("./BookCopy").resolvers,
-  require("./ExternalID").resolvers,
-  require("./Resource").resolvers,
-  require("./Timestampable").resolvers,
-  require("./User").resolvers
-]);
-
-export const schemaWithResolvers = addResolversToSchema({
-  schema,
-  resolvers,
+export const rootSchema = addResolversToSchema({
+  schema: loadSchemaSync(path.join(__dirname, "./**/*.graphql"), {
+    loaders: [new GraphQLFileLoader()]
+  }),
+  resolvers: mergeResolvers<Context, Resolvers<Context>>([
+    require("./Anything").resolvers,
+    require("./Author").resolvers,
+    require("./Book").resolvers,
+    require("./BookCopy").resolvers,
+    require("./ExternalID").resolvers,
+    require("./Resource").resolvers,
+    require("./Timestampable").resolvers,
+    require("./User").resolvers
+  ]),
   inheritResolversFromInterfaces: true
 });
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,12 +4,12 @@ import { Connection } from "typeorm";
 import { ASSETS_BASE_URL } from "./config";
 import { buildAuthorsLoader } from "./database/authorsLoader";
 import { User } from "./database/entity/User";
-import { schemaWithResolvers } from "./graphql/schema";
+import { rootSchema } from "./graphql/schema";
 import { Context } from "./types";
 
 export const createServer = (connection: Connection) =>
   new ApolloServer({
-    schema: schemaWithResolvers,
+    schema: rootSchema,
     context: async (): Promise<Context> => {
       const currentUser = await connection.manager.findOne(User, {
         name: "Bob"
