2add4246728976a17b2d0be474ad4274db930969
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-25 10:48:23 +0200

    Refactor getTestClient

--- packages/server/src/tests/hepers.ts
+++ packages/server/src/tests/hepers.ts
@@ -7,21 +7,19 @@ import { getConnection } from "typeorm";
 
 import { ASSETS_BASE_URL } from "../config";
 import { buildAuthorsLoader } from "../database/authorsLoader";
-import { User } from "../database/entity/User";
 import { rootSchema } from "../graphql/schema";
+import { Context } from "../types";
 
-export const getTestClient = ({
-  currentUser
-}: {
-  currentUser?: User;
-} = {}): ApolloServerTestClient => {
+export const getTestClient = (
+  context: Partial<Context> = {}
+): ApolloServerTestClient => {
   const server = new ApolloServer({
     schema: rootSchema,
     context: {
+      ...context,
       assetsBaseUrl: ASSETS_BASE_URL,
       authorsLoader: buildAuthorsLoader(),
-      connection: getConnection(),
-      currentUser
+      connection: getConnection()
     }
   });
 
