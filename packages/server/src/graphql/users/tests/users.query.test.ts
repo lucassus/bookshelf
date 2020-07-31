3345369b307ee4d4b7961149cffc57f457ac4fac
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-31 12:06:24 +0200

    Refactor with jest.each table WIP

--- packages/server/src/graphql/users/tests/users.query.test.ts
+++ packages/server/src/graphql/users/tests/users.query.test.ts
@@ -55,44 +55,45 @@ describe("users query", () => {
     });
   });
 
-  // TODO: Provide a better description
-  // TODO: Consider refactor with a table
   test.each<[undefined | Role, boolean, undefined | string]>([
     [undefined, false, "Unauthorized access! Please log in."],
     [Role.User, false, "Unauthorized access! Please log in as admin."],
     [Role.Admin, true, undefined]
-  ])("fetching email fields 2", async (role, success, expectedErrorMessage) => {
-    // Given
-    const currentUser = role
-      ? await createUser({ isAdmin: role === Role.Admin })
-      : undefined;
-    const otherUser = await createUser();
+  ])(
+    "fetching email fields for %s role",
+    async (role, success, expectedErrorMessage) => {
+      // Given
+      const currentUser = role
+        ? await createUser({ isAdmin: role === Role.Admin })
+        : undefined;
+      const otherUser = await createUser();
 
-    // When
-    const res = await createTestClient({ currentUser }).query({
-      query: gql`
-        query {
-          users {
-            id
-            email
+      // When
+      const res = await createTestClient({ currentUser }).query({
+        query: gql`
+          query {
+            users {
+              id
+              email
+            }
           }
-        }
-      `
-    });
-
-    if (success && currentUser) {
-      expect(res.errors).toBe(undefined);
-
-      expect(res.data).not.toBe(null);
-      expect(res.data).toMatchObject({
-        users: [
-          { id: expect.any(String), email: currentUser.email },
-          { id: expect.any(String), email: otherUser.email }
-        ]
+        `
       });
-    } else {
-      expect(res.errors).not.toBe(undefined);
-      expect(res.errors![0].message).toBe(expectedErrorMessage);
+
+      if (success && currentUser) {
+        expect(res.errors).toBe(undefined);
+
+        expect(res.data).not.toBe(null);
+        expect(res.data).toMatchObject({
+          users: [
+            { id: expect.any(String), email: currentUser.email },
+            { id: expect.any(String), email: otherUser.email }
+          ]
+        });
+      } else {
+        expect(res.errors).not.toBe(undefined);
+        expect(res.errors![0].message).toBe(expectedErrorMessage);
+      }
     }
-  });
+  );
 });
