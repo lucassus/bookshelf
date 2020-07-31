6aff8720510aa0709d06640f7aa1b6e4443ed796
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-31 14:38:04 +0200

    Parametrize test for login mutation

--- packages/server/src/graphql/authentication/tests/login.mutation.test.ts
+++ packages/server/src/graphql/authentication/tests/login.mutation.test.ts
@@ -1,95 +1,55 @@
 import { gql } from "apollo-server-express";
 
-import { User } from "../../../database/entity/User";
 import { createTestClient } from "../../../testUtils/createTestClient";
 import { createUser } from "../../../testUtils/factories";
 
 describe("login mutation", () => {
-  let user: User;
-  const validPassword = "secret password";
+  const validEmail = "valid@email.com";
+  const validPassword = "valid password";
 
-  beforeEach(async () => {
-    user = await createUser({
-      email: "user@exmaple.com",
-      password: validPassword
-    });
-  });
+  test.each([
+    [{ email: validEmail, password: validPassword }, true, "Login success!"],
+    [
+      { email: "invalid@email.com", password: validPassword },
+      false,
+      "Invalid email or password!"
+    ],
+    [
+      { email: validEmail, password: "invalid password" },
+      false,
+      "Invalid email or password!"
+    ]
+  ])(
+    "for %o responds with success: %s and message: %s",
+    async (input, success, message) => {
+      // Given
+      await createUser({ email: validEmail, password: validPassword });
 
-  const LoginMutation = gql`
-    mutation($input: LoginInput!) {
-      login(input: $input) {
-        success
-        message
-        authToken
-      }
+      // When
+      const res = await createTestClient().mutate({
+        mutation: gql`
+          mutation($input: LoginInput!) {
+            login(input: $input) {
+              success
+              message
+              authToken
+            }
+          }
+        `,
+        variables: {
+          input
+        }
+      });
+
+      // Then
+      expect(res.errors).toBe(undefined);
+      expect(res.data).toMatchObject({
+        login: {
+          success,
+          message,
+          authToken: success ? expect.any(String) : null
+        }
+      });
     }
-  `;
-
-  test("login with valid credentials", async () => {
-    // When
-    const res = await createTestClient().mutate({
-      mutation: LoginMutation,
-      variables: {
-        input: {
-          email: user.email,
-          password: validPassword
-        }
-      }
-    });
-
-    // Then
-    expect(res.errors).toBe(undefined);
-    expect(res.data).toMatchObject({
-      login: {
-        success: true,
-        authToken: expect.any(String)
-      }
-    });
-  });
-
-  test("login with invalid email", async () => {
-    // When
-    const res = await createTestClient().mutate({
-      mutation: LoginMutation,
-      variables: {
-        input: {
-          email: "invalid@email.com",
-          password: validPassword
-        }
-      }
-    });
-
-    // Then
-    expect(res.errors).toBe(undefined);
-    expect(res.data).toMatchObject({
-      login: {
-        success: false,
-        message: "Invalid email or password!",
-        authToken: null
-      }
-    });
-  });
-
-  test("login with invalid password", async () => {
-    // When
-    const res = await createTestClient().mutate({
-      mutation: LoginMutation,
-      variables: {
-        input: {
-          email: user.email,
-          password: "invalid password"
-        }
-      }
-    });
-
-    // Then
-    expect(res.errors).toBe(undefined);
-    expect(res.data).toMatchObject({
-      login: {
-        success: false,
-        message: "Invalid email or password!",
-        authToken: null
-      }
-    });
-  });
+  );
 });
