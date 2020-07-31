005a4b170906a196d3eeec71525f37ebc24bc0a6
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-07-31 10:21:17 +0200

    Write failing test for fetching users along with emails

--- packages/server/src/graphql/users/tests/users.query.test.ts
+++ packages/server/src/graphql/users/tests/users.query.test.ts
@@ -3,33 +3,110 @@ import { gql } from "apollo-server-express";
 import { createTestClient } from "../../../testUtils/createTestClient";
 import { createUser } from "../../../testUtils/factories";
 
-test("users query", async () => {
-  // Given
-  await createUser();
-  await createUser();
-  await createUser();
+describe("users query", () => {
+  it("fetches users", async () => {
+    // Given
+    const user1 = await createUser({ name: "Alice" });
+    const user2 = await createUser({ name: "Bob" });
+    const user3 = await createUser({ name: "Luke" });
 
-  // When
-  const res = await createTestClient().query({
-    query: gql`
-      query {
-        users {
-          name
-          info
-          avatar {
-            image {
-              path
-              url
+    // When
+    const res = await createTestClient().query({
+      query: gql`
+        query {
+          users {
+            id
+            name
+            info
+            avatar {
+              image {
+                path
+                url
+              }
+              color
             }
-            color
           }
         }
-      }
-    `
+      `
+    });
+
+    // Then
+    expect(res.errors).toBe(undefined);
+    expect(res.data).not.toBe(null);
+    expect(res.data).toMatchObject({
+      users: [
+        {
+          id: expect.any(String),
+          name: user1.name,
+          info: user1.info
+        },
+        {
+          id: expect.any(String),
+          name: user2.name,
+          info: user2.info
+        },
+        {
+          id: expect.any(String),
+          name: user3.name,
+          info: user3.info
+        }
+      ]
+    });
   });
 
-  // Then
-  expect(res.errors).toBe(undefined);
-  expect(res.data).not.toBe(null);
-  expect(res.data!.users).toMatchSnapshot();
+  // TODO: Idea: refactor with jest.each
+  describe("fetching email fields", () => {
+    const GetUsersWithEmails = gql`
+      query {
+        users {
+          id
+          name
+          email
+        }
+      }
+    `;
+
+    it("return users with emails when authenticated as admin", async () => {
+      // Given
+      const currentUser = await createUser({ isAdmin: true });
+      const user = await createUser();
+
+      // When
+      const res = await createTestClient({ currentUser }).query({
+        query: GetUsersWithEmails
+      });
+
+      // Then
+      expect(res.errors).toBe(undefined);
+      expect(res.data).not.toBe(null);
+      expect(res.data).toMatchObject({
+        users: [
+          {
+            id: expect.any(String),
+            name: currentUser.name,
+            email: currentUser.email
+          },
+          {
+            id: expect.any(String),
+            name: user.name,
+            email: user.email
+          }
+        ]
+      });
+    });
+
+    it("returns an error when not authenticated as admin", async () => {
+      // Given
+      const currentUser = await createUser({ isAdmin: false });
+
+      // When
+      const res = await createTestClient({ currentUser }).query({
+        query: GetUsersWithEmails
+      });
+
+      // Then
+      expect(res.errors).not.toBe(undefined);
+      console.log(res.errors);
+    });
+  });
 });
