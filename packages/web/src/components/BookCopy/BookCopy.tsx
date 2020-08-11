6442d38a00a86569096ba449a73c9c2699986f10
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-11 14:36:24 +0200

    Hide borrow book button when user is not authenticated

--- packages/web/src/components/BookCopy/BookCopy.tsx
+++ packages/web/src/components/BookCopy/BookCopy.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useAuth } from "../AuthContext";
 import { Avatar } from "../Avatar";
 import { Actions } from "./Actions";
 import { BookCopyFragment } from "./BookCopy.fragment.generated";
@@ -10,36 +11,40 @@ type Props = {
   bookCopy: BookCopyFragment;
 };
 
-export const BookCopy: React.FunctionComponent<Props> = ({ bookCopy }) => (
-  <div className={styles.container}>
-    <div className={styles.bookCoverWithAvatars}>
-      <Link to={`/books/${bookCopy.book.id}`}>
-        <img src={bookCopy.book.cover.url} alt={bookCopy.book.title} />
-      </Link>
+export const BookCopy: React.FunctionComponent<Props> = ({ bookCopy }) => {
+  const { currentUser } = useAuth();
 
-      <div className={styles.ownerAvatar}>
-        <Link to={`/users/${bookCopy.owner.id}`}>
-          <Avatar
-            name={bookCopy.owner.name}
-            small
-            avatar={bookCopy.owner.avatar}
-          />
+  return (
+    <div className={styles.container}>
+      <div className={styles.bookCoverWithAvatars}>
+        <Link to={`/books/${bookCopy.book.id}`}>
+          <img src={bookCopy.book.cover.url} alt={bookCopy.book.title} />
         </Link>
-      </div>
 
-      {bookCopy.borrower && (
-        <div className={styles.borrowerAvatar}>
-          <Link to={`/users/${bookCopy.borrower.id}`}>
+        <div className={styles.ownerAvatar}>
+          <Link to={`/users/${bookCopy.owner.id}`}>
             <Avatar
-              name={bookCopy.borrower.name}
+              name={bookCopy.owner.name}
               small
-              avatar={bookCopy.borrower.avatar}
+              avatar={bookCopy.owner.avatar}
             />
           </Link>
         </div>
-      )}
-    </div>
 
-    <Actions bookCopy={bookCopy} />
-  </div>
-);
+        {bookCopy.borrower && (
+          <div className={styles.borrowerAvatar}>
+            <Link to={`/users/${bookCopy.borrower.id}`}>
+              <Avatar
+                name={bookCopy.borrower.name}
+                small
+                avatar={bookCopy.borrower.avatar}
+              />
+            </Link>
+          </div>
+        )}
+      </div>
+
+      {currentUser && <Actions bookCopy={bookCopy} />}
+    </div>
+  );
+};
