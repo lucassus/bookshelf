44c444b2bd7744a44d30826f2d01eea3e95e2f1c
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-17 16:01:20 +0200

    Chore: Imporove codegen setup

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "apollo-server-express";
 
 import { authenticateRequest } from "./common/authentication";
+import { Environment } from "./config";
 import { buildContext, Context } from "./graphql/context";
 import { rootSchema } from "./graphql/rootSchema";
 
@@ -11,6 +12,7 @@ export const createServer = () =>
       const currentUser = await authenticateRequest(req);
       return buildContext({ req, res, currentUser });
     },
+    debug: process.env.ENV === Environment.development,
     introspection: true,
     playground: true
   });
