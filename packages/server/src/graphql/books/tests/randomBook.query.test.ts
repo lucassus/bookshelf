ac22844cf3c41b0206ed8900f51e24d1c0e8b217
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-20 15:11:21 +0200

    Mock BooksService in test

--- packages/server/src/graphql/books/tests/randomBook.query.test.ts
+++ packages/server/src/graphql/books/tests/randomBook.query.test.ts
@@ -1,16 +1,21 @@
 import { gql } from "apollo-server-express";
+import { Container } from "typedi";
 
 import { createTestClient } from "../../../testUtils/createTestClient";
-import { createAuthor, createBook } from "../../../testUtils/factories";
+import { BooksService } from "../BooksService";
 
 test("randomBook query", async () => {
   // Given
-  const author = await createAuthor({ name: "Andrzej Sapkowski" });
-  const book = await createBook({
-    author,
+  const book = {
+    id: "1234",
     title: "The tower of the swallow",
     coverPath: "/images/book-covers/witcher4.jpg"
-  });
+  };
+
+  const fakeBooksService: Partial<BooksService> = {
+    findRandom: jest.fn().mockResolvedValue(book)
+  };
+  Container.set(BooksService, fakeBooksService);
 
   // When
   const res = await createTestClient().query({
@@ -30,7 +35,6 @@ test("randomBook query", async () => {
 
   // Then
   expect(res.errors).toBe(undefined);
-  expect(res.data).not.toBeNull();
   expect(res.data).toMatchObject({
     randomBook: {
       id: expect.any(String),
