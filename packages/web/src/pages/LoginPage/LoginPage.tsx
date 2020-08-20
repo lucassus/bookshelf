48b4ca417cdb17710c5c395ef55e111bcf1b37f1
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-20 14:17:55 +0200

    Add some login form validations

--- packages/web/src/pages/LoginPage/LoginPage.tsx
+++ packages/web/src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,7 @@
+import { yupResolver } from "@hookform/resolvers";
 import React from "react";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
 
 import { useAuth } from "../../components/AuthContext";
 import { useLoginMutation } from "./Login.mutation.generated";
@@ -10,8 +12,15 @@ type Inputs = {
   password: string;
 };
 
+const schema = yup.object().shape({
+  email: yup.string().required().email(),
+  password: yup.string().required().min(6)
+});
+
 export const LoginPage: React.FunctionComponent = () => {
-  const { register, handleSubmit } = useForm<Inputs>();
+  const { register, handleSubmit, errors } = useForm<Inputs>({
+    resolver: yupResolver(schema)
+  });
   const [login, { loading }] = useLoginMutation();
   const { authorize } = useAuth();
 
@@ -37,6 +46,7 @@ export const LoginPage: React.FunctionComponent = () => {
         <div>
           <label htmlFor="email-field">Email</label>
           <input type="text" id="email-field" name="email" ref={register} />
+          <p>{errors.email?.message}</p>
         </div>
 
         <div>
@@ -47,6 +57,7 @@ export const LoginPage: React.FunctionComponent = () => {
             name="password"
             ref={register}
           />
+          <p>{errors.password?.message}</p>
         </div>
 
         <button type="submit" disabled={loading}>
