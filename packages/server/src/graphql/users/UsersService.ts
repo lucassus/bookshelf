2b9bdbd8917fc65667739bb2084b4987f4391019
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-20 16:46:07 +0200

    Implement UsersService

---
+++ packages/server/src/graphql/users/UsersService.ts
@@ -0,0 +1,64 @@
+import { Service } from "typedi";
+import { Connection, Repository } from "typeorm";
+import { InjectConnection, InjectRepository } from "typeorm-typedi-extensions";
+
+import { hashPassword } from "../../common/passwords";
+import { Avatar, User } from "../../database/entity";
+
+@Service()
+export class UsersService {
+  @InjectConnection()
+  private connection: Connection;
+
+  @InjectRepository(User)
+  private repository: Repository<User>;
+
+  findAll() {
+    return this.repository.find({ order: { name: "ASC" } });
+  }
+
+  findOneByIdOrFail(id: string | number) {
+    return this.repository.findOneOrFail(id);
+  }
+
+  async create(
+    userAttributes: Partial<User> & { password: string },
+    avatarAttributes: Partial<Avatar>
+  ): Promise<User> {
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+
+    try {
+      await queryRunner.startTransaction();
+
+      const avatar = queryRunner.manager.create(Avatar, avatarAttributes);
+      await queryRunner.manager.save(avatar);
+
+      const user = queryRunner.manager.create(User, {
+        ...userAttributes,
+        passwordHash: hashPassword(userAttributes.password),
+        avatar
+      });
+      await queryRunner.manager.save(user);
+
+      return user;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async update(
+    id: string | number,
+    userAttributes: Partial<User>
+  ): Promise<User> {
+    const user = await this.findOneByIdOrFail(id);
+    return this.repository.save(this.repository.merge(user, userAttributes));
+  }
+
+  delete(id: string | number) {
+    return this.repository.delete(id);
+  }
+}
--- packages/server/src/graphql/users/resolvers.ts
+++ packages/server/src/graphql/users/resolvers.ts
@@ -1,58 +1,35 @@
-import { hashPassword } from "../../common/passwords";
-import { Avatar, User } from "../../database/entity";
 import { Context } from "../context";
 import { Resolvers } from "../resolvers-types.generated";
+import { UsersService } from "./UsersService";
 
 const resolvers: Resolvers<Context> = {
   Query: {
-    users: (rootValue, args, { connection }) =>
-      connection.getRepository(User).find({ order: { name: "ASC" } }),
+    users: (rootValue, args, { container }) =>
+      container.get(UsersService).findAll(),
 
-    user: (rootValue, { id }, { connection }) =>
-      connection.manager.findOneOrFail(User, id)
+    user: (rootValue, { id }, { container }) =>
+      container.get(UsersService).findOneByIdOrFail(id)
   },
 
   Mutation: {
-    createUser: async (rootValue, args, { connection }) => {
+    createUser: async (rootValue, args, { container }) => {
       const { avatar: avatarAttributes, ...userAttributes } = args.input;
 
-      const queryRunner = connection.createQueryRunner();
-      await queryRunner.connect();
+      const user = await container
+        .get(UsersService)
+        .create(userAttributes, avatarAttributes);
 
-      try {
-        await queryRunner.startTransaction();
-
-        const avatar = queryRunner.manager.create(Avatar, avatarAttributes);
-        await queryRunner.manager.save(avatar);
-
-        // TODO: Add validations for userAttributes, like email, password min length etc
-        const user = queryRunner.manager.create(User, {
-          ...userAttributes,
-          passwordHash: hashPassword(userAttributes.password),
-          avatar
-        });
-        await queryRunner.manager.save(user);
-
-        return {
-          success: true,
-          message: "User was successfully created.",
-          user
-        };
-      } catch (error) {
-        await queryRunner.rollbackTransaction();
-        throw error;
-      } finally {
-        await queryRunner.release();
-      }
+      return {
+        success: true,
+        message: "User was successfully created.",
+        user
+      };
     },
 
-    updateUser: async (rootValue, args, { connection }) => {
+    updateUser: async (rootValue, args, { container }) => {
       const { id, ...userAttributes } = args.input;
 
-      const user = await connection.manager.findOneOrFail(User, id);
-      await connection.manager.save(
-        connection.manager.merge(User, user, userAttributes)
-      );
+      const user = await container.get(UsersService).update(id, userAttributes);
 
       return {
         success: true,
@@ -61,8 +38,8 @@ const resolvers: Resolvers<Context> = {
       };
     },
 
-    deleteUser: async (rootValue, { id }, { connection }) => {
-      await connection.manager.delete(User, { id });
+    deleteUser: async (rootValue, { id }, { container }) => {
+      await container.get(UsersService).delete(id);
 
       return {
         success: true,
