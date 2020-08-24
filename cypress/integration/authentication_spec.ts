b3e564b212c2c51f94dc6a3c7bf55ccf9663e85c
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-24 10:47:25 +0200

    Improve e2e for authentication flow

--- cypress/integration/authentication_spec.ts
+++ cypress/integration/authentication_spec.ts
@@ -1,13 +1,25 @@
-it("handles authentication flow", () => {
+it("handles the authentication flow", () => {
   cy.visit("/");
   cy.contains("Login").click();
 
   cy.get("form").within(() => {
-    cy.get('input[name="email"]').type("bob@example.com");
+    cy.contains("Login").click();
+    cy.contains("email is a required field");
+    cy.contains("password is a required field");
+
+    cy.get('input[name="email"]').type("invalid");
+    cy.contains("email must be a valid email");
+
+    cy.get('input[name="email"]').clear().type("bob@example.com");
     cy.get('input[name="password"]').type("password");
+
     cy.contains("Login").click();
   });
 
   cy.contains("You are logged in as Bob");
+
+  cy.reload();
+  cy.contains("You are logged in as Bob");
+
   cy.contains("Logout").click();
 });
