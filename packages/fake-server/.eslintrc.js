292380017b3fff5c12aa8f8d2b40a885cbee081e
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-30 18:13:01 +0200

    Simplify eslint configuration

--- packages/fake-server/.eslintrc.js
+++ packages/fake-server/.eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: "../../.eslintrc.js",
-  overrides: [
-    {
-      files: ["*.test.ts"],
-      env: { jest: true },
-      extends: ["plugin:jest/recommended"]
-    }
-  ],
-  rules: {
-    "@typescript-eslint/no-var-requires": "off",
-    "global-require": "off",
-    "import/no-extraneous-dependencies": [
-      "error",
-      { devDependencies: ["**/*.test.ts", "src/testUtils/**/*.ts"] }
-    ]
-  }
+  extends: "../../.eslintrc.js"
 };
