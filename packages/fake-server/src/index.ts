9e2d774c98598c380645fd7af2de2a0145e09957
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-08-31 16:37:22 +0200

    Minor cleanup

--- packages/fake-server/src/index.ts
+++ packages/fake-server/src/index.ts
@@ -1,41 +1,27 @@
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import { ApolloServer } from "apollo-server";
-import faker from "faker";
 import { buildClientSchema } from "graphql";
 
 import { ASSETS_BASE_URL, PORT } from "./config";
 import { mocks } from "./mocks";
+import { resolvers } from "./resolvers";
 import introspectionResult from "./schema.json";
 
-const schema = buildClientSchema(introspectionResult as any);
-
 const schemaWithResolvers = addResolversToSchema({
-  schema,
-  resolvers: {
-    Timestampable: {
-      createdAt: () =>
-        faker.date.between(new Date(2018, 0, 1), new Date()).toISOString(),
-      updatedAt: () =>
-        faker.date.between(new Date(2018, 0, 1), new Date()).toISOString()
-    },
-    Person: {
-      name: () => faker.name.findName(),
-      email: () => faker.internet.email(),
-      info: () => faker.lorem.paragraph()
-    }
-  },
+  schema: buildClientSchema(introspectionResult as any),
+  resolvers,
   inheritResolversFromInterfaces: true
 });
 
-const schemaWithMocks = addMocksToSchema({
+const schema = addMocksToSchema({
   schema: schemaWithResolvers,
   mocks,
   preserveResolvers: true
 });
 
 const server = new ApolloServer({
-  schema: schemaWithMocks,
+  schema,
   context: {
     assetsBaseUrl: ASSETS_BASE_URL
   }
