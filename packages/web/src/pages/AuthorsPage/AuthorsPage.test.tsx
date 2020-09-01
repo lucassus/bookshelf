95e5420d96cf16223faea5aca4ccc14b6af3e6a9
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-01 08:54:31 +0200

    Initial implementation of ApolloMockedProvider

--- packages/web/src/pages/AuthorsPage/AuthorsPage.test.tsx
+++ packages/web/src/pages/AuthorsPage/AuthorsPage.test.tsx
@@ -1,78 +1,57 @@
-import { render, waitForElementToBeRemoved } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved
+} from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router";
 
 import { ApolloMockedProvider } from "../../testUtils/ApolloMockedProvider";
 import { AuthorsPage } from "./AuthorsPage";
 
-// TODO: Refactor with graphql mock
-// export const GetAuthorsDocumentMock: MockedResponse<{
-//   authors: Author[];
-// }> = {
-//   request: {
-//     query: GetAuthorsDocument
-//   },
-//   result: {
-//     data: {
-//       authors: [
-//         {
-//           __typename: "Author",
-//           ...createAuthor({ id: "1", name: "J. K. Rowling" })
-//         },
-//         {
-//           __typename: "Author",
-//           ...createAuthor({ id: "2", name: "Andrzej Sapkowski" })
-//         }
-//       ]
-//     }
-//   }
-// };
-//
-// export const GetAuthorsDocumentErrorMock: MockedResponse = {
-//   request: {
-//     query: GetAuthorsDocument
-//   },
-//   error: new Error()
-// };
-
 describe("<AuthorsPage />", () => {
   const renderComponent = () =>
     render(
-      <ApolloMockedProvider>
+      <ApolloMockedProvider
+        customResolvers={{
+          Query: () => ({
+            authors: () => [
+              { name: "J. K. Rowling" },
+              { name: "Andrzej Sapkowski" }
+            ]
+          })
+        }}
+      >
         <MemoryRouter>
           <AuthorsPage />
         </MemoryRouter>
       </ApolloMockedProvider>
     );
-  // const renderComponent = ({ mocks }: { mocks: MockedResponse[] }) =>
-  //   render(
-  //     <MockedProvider mocks={mocks}>
-  //       <MemoryRouter>
-  //         <AuthorsPage />
-  //       </MemoryRouter>
-  //     </MockedProvider>
-  //   );
 
   it("renders list of authors", async () => {
     // When
-    const { getByText } = renderComponent();
+    renderComponent();
 
     // Then
-    expect(getByText("Loading authors...")).toBeInTheDocument();
-    await waitForElementToBeRemoved(() => getByText("Loading authors..."));
+    expect(screen.getByText("Loading authors...")).toBeInTheDocument();
+    await waitForElementToBeRemoved(() =>
+      screen.getByText("Loading authors...")
+    );
 
-    expect(getByText("J. K. Rowling")).toBeInTheDocument();
-    expect(getByText("Andrzej Sapkowski")).toBeInTheDocument();
+    screen.debug();
+
+    expect(screen.getByText("J. K. Rowling")).toBeInTheDocument();
+    expect(screen.getByText("Andrzej Sapkowski")).toBeInTheDocument();
   });
 
   // xit("renders error alert when request fails", async () => {
   //   // When
-  //   const { getByText } = renderComponent({
+  //   renderComponent({
   //     mocks: [GetAuthorsDocumentErrorMock]
   //   });
   //
   //   // Then
-  //   await waitForElementToBeRemoved(() => getByText("Loading authors..."));
-  //   expect(getByText("Could not load authors...")).toBeInTheDocument();
+  //   await waitForElementToBeRemoved(() => screen.getByText("Loading authors..."));
+  //   expect(screen.getByText("Could not load authors...")).toBeInTheDocument();
   // });
 });
--- packages/web/src/testUtils/ApolloMockedProvider.tsx
+++ packages/web/src/testUtils/ApolloMockedProvider.tsx
@@ -4,6 +4,7 @@ import {
   ApolloProvider,
   InMemoryCache
 } from "@apollo/client";
+import { mergeResolvers } from "@graphql-tools/merge";
 import { addMocksToSchema } from "@graphql-tools/mock";
 import { addResolversToSchema } from "@graphql-tools/schema";
 import { SchemaLink } from "apollo-link-schema";
@@ -14,31 +15,40 @@ import introspectionResult from "../schema.generated.json";
 import { mocks } from "./mocks";
 import { resolvers } from "./resolvers";
 
-// TODO: Figure out how to dry it
-const schemaWithResolvers = addResolversToSchema({
-  schema: buildClientSchema(introspectionResult as any),
-  resolvers,
-  inheritResolversFromInterfaces: true
-});
+type Props = {
+  // TODO: Better typing
+  customResolvers?: any;
+};
 
-const schema = addMocksToSchema({
-  schema: schemaWithResolvers,
-  mocks,
-  preserveResolvers: true
-});
+// TODO: Figure out how to dry it, see @bookshelf/fake-server
+export const ApolloMockedProvider: React.FunctionComponent<Props> = ({
+  customResolvers = {},
+  children
+}) => {
+  const schemaWithResolvers = addResolversToSchema({
+    schema: buildClientSchema(introspectionResult as any),
+    resolvers,
+    inheritResolversFromInterfaces: true
+  });
 
-const client = new ApolloClient({
-  // TODO: See https://github.com/apollographql/apollo-link/issues/1258#issuecomment-604031488
-  link: (new SchemaLink({
-    schema,
-    context: {
-      assetsBaseUrl:
-        "https://res.cloudinary.com/lucassus/image/upload/bookshelf"
-    }
-  }) as unknown) as ApolloLink,
-  cache: new InMemoryCache()
-});
+  const schema = addMocksToSchema({
+    schema: schemaWithResolvers,
+    mocks: mergeResolvers([mocks, customResolvers]),
+    preserveResolvers: true
+  });
 
-export const ApolloMockedProvider: React.FunctionComponent = ({ children }) => (
-  <ApolloProvider client={client}>{children}</ApolloProvider>
-);
+  const client = new ApolloClient({
+    // TODO: See https://github.com/apollographql/apollo-link/issues/1258#issuecomment-604031488
+    link: (new SchemaLink({
+      schema,
+      context: {
+        // TODO: Dry it
+        assetsBaseUrl:
+          "https://res.cloudinary.com/lucassus/image/upload/bookshelf"
+      }
+    }) as unknown) as ApolloLink,
+    cache: new InMemoryCache()
+  });
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
