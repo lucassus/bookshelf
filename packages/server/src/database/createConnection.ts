87461a214ba23107bef5cf97d4f9fa44ca00f774
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-08 09:16:55 +0200

    Fix running tests

--- packages/server/src/database/createConnection.ts
+++ packages/server/src/database/createConnection.ts
@@ -1,18 +1,14 @@
-import findUp from "find-up";
-import { dirname } from "path";
 import {
   ConnectionOptionsReader,
   createConnection as typeormCreateConnection
 } from "typeorm";
 
-// TODO: Refactor it
 export const createConnection = async () => {
-  const path = await findUp("ormconfig.js");
-  if (!path) {
-    throw new Error("Unable to find ormconfig.js");
+  let root = process.cwd();
+  if (!root.includes("/packages/server")) {
+    root = root + "/packages/server";
   }
 
-  const root = dirname(path);
   const optionsReader = new ConnectionOptionsReader({ root });
   const options = await optionsReader.get("default");
 
