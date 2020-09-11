249ce84de7436b8150ca84d4659f502f3605fdab
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-11 08:33:32 +0200

    Improve db seed performance

--- packages/server/src/rest/api/seed.ts
+++ packages/server/src/rest/api/seed.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import { getConnection } from "typeorm";
 
+import { Author, Avatar, Book, BookCopy, User } from "../../database/entity";
 import { loadFixtures } from "../../testUtils/fixtures";
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
-  await getConnection().synchronize(true);
+  const connection = getConnection();
+
+  const entities = [Book, BookCopy, Author, User, Avatar];
+  await connection.query(
+    `TRUNCATE TABLE ${entities
+      .map((entity) => connection.getMetadata(entity).tableName)
+      .join(", ")};`
+  );
+
   await loadFixtures();
 
   res.send(200);
