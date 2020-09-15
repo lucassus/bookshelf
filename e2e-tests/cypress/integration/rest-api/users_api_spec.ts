270f745fe06cfb8b63d53627294cdb665d773dd5
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-15 16:21:23 +0200

    Add a test case for 401 response

--- e2e-tests/cypress/integration/rest-api/users_api_spec.ts
+++ e2e-tests/cypress/integration/rest-api/users_api_spec.ts
@@ -1,10 +1,22 @@
 describe("/api/users", () => {
-  beforeEach(() => {
-    cy.login();
-    cy.request("/api/users").as("users");
+  describe("when authenticated", () => {
+    beforeEach(() => {
+      cy.login();
+      cy.request("/api/users").as("users");
+    });
+
+    it("responds with success", () => {
+      cy.get("@users").its("status").should("equal", 200);
+    });
   });
 
-  it("responds with success", () => {
-    cy.get("@users").its("status").should("equal", 200);
+  describe("when not authenticated", () => {
+    beforeEach(() => {
+      cy.request({ url: "/api/users", failOnStatusCode: false }).as("users");
+    });
+
+    it("responds with error", () => {
+      cy.get("@users").its("status").should("equal", 401);
+    });
   });
 });
