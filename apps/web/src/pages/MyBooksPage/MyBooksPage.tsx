f9af334f8a50660a1ea117e186b340934a0b9c42
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-18 11:46:09 +0200

    Simple owned books page WIP

--- apps/web/src/pages/MyBooksPage/MyBooksPage.tsx
+++ apps/web/src/pages/MyBooksPage/MyBooksPage.tsx
@@ -1,34 +1,46 @@
 import React from "react";
 
 import { BookCopy } from "../../components/BookCopy";
-import { useGetOwnedBookCopiesQuery } from "./GetOwnedBookCopies.query.generated";
+import { ErrorAlert } from "../../components/ErrorAlert";
+import { useGetMyBookCopiesQuery } from "./GetMyBookCopies.query.generated";
 import styles from "./MyBooksPage.module.scss";
 
 export const MyBooksPage: React.FunctionComponent = () => {
-  const {
-    data,
-    loading: loadingOwnedBookCopies
-  } = useGetOwnedBookCopiesQuery();
+  const { data, error, loading } = useGetMyBookCopiesQuery();
+
+  if (loading) {
+    return <span>Loading...</span>;
+  }
+
+  if (error || !data) {
+    return <ErrorAlert message="Could not load books..." />;
+  }
+
+  // TODO: Find a better solution for loading my book copies
+  const currentUser = data.currentUser!;
+  const { ownedBookCopies, borrowedBookCopies } = currentUser;
 
   return (
     <div>
-      {loadingOwnedBookCopies ? (
-        <span>Loading owned book copies</span>
-      ) : (
-        data && (
-          <div>
-            <h2>
-              Owned book copies ({data.currentUser?.ownedBookCopies.length})
-            </h2>
+      <div>
+        <h2>Owned book copies ({ownedBookCopies.length})</h2>
 
-            <div className={styles.bookCopies}>
-              {data.currentUser?.ownedBookCopies.map((bookCopy) => (
-                <BookCopy key={bookCopy.id} bookCopy={bookCopy} />
-              ))}
-            </div>
-          </div>
-        )
-      )}
+        <div className={styles.bookCopies}>
+          {ownedBookCopies.map((bookCopy) => (
+            <BookCopy key={bookCopy.id} bookCopy={bookCopy} />
+          ))}
+        </div>
+      </div>
+
+      <div>
+        <h2>Borrowed book copies ({borrowedBookCopies.length})</h2>
+
+        <div className={styles.bookCopies}>
+          {borrowedBookCopies.map((bookCopy) => (
+            <BookCopy key={bookCopy.id} bookCopy={bookCopy} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
