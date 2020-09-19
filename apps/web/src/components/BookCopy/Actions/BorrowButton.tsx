604cb10b83f66065af954cfde1f69293912e07c6
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-19 11:30:45 +0200

    wip

--- apps/web/src/components/BookCopy/Actions/BorrowButton.tsx
+++ apps/web/src/components/BookCopy/Actions/BorrowButton.tsx
@@ -9,8 +9,32 @@ type Props = {
 
 export const BorrowButton: React.FunctionComponent<Props> = ({ bookCopy }) => {
   const [borrowBookCopy, { loading }] = useBorrowBookCopyMutation();
+  // const { currentUser } = useAuth();
 
-  const handleClick = () => borrowBookCopy({ variables: { id: bookCopy.id } });
+  const handleClick = () =>
+    borrowBookCopy({
+      variables: { id: bookCopy.id }
+      // TODO: WIP
+      // update(cache, { data }) {
+      //   if (!data || data.borrowBookCopy.__typename !== "BookCopy") {
+      //     return;
+      //   }
+      //
+      //   cache.modify({
+      //     id: cache.identify(currentUser!),
+      //     fields: {
+      //       borrowedBookCopies(refs) {
+      //         const newRef = cache.writeFragment({
+      //           data: data.borrowBookCopy,
+      //           fragment: BookCopyFragmentDoc
+      //         });
+      //
+      //         return [...refs, newRef];
+      //       }
+      //     }
+      //   });
+      // }
+    });
 
   return (
     <button disabled={loading} onClick={handleClick}>
