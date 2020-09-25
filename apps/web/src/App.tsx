2df8875dcb72cfdf8a8573b317187a42711e9717
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-25 08:40:14 +0200

    Create AppRoutes component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,22 +1,10 @@
-import "./App.scss";
-
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import "./App.scss";
 
 import { AppTopBar } from "./components/AppTopBar";
 import { useCurrentUser } from "./components/CurrentUserProvider";
 import { ErrorBoundary } from "./components/ErrorBoundary";
-import { AuthorDetailsPage } from "./pages/AuthorDetailsPage";
-import { AuthorsPage } from "./pages/AuthorsPage";
-import { BookDetailsPage } from "./pages/BookDetailsPage";
-import { BooksPage } from "./pages/BooksPage";
-import { LoginPage } from "./pages/LoginPage";
-import { MyBooksPage } from "./pages/MyBooksPage";
-import { NotFoundPage } from "./pages/NotFoundPage";
-import { ProfilePage } from "./pages/ProfilePage";
-import { SignupPage } from "./pages/SignupPage";
-import { UserDetailsPage } from "./pages/UserDetailsPage";
-import { UsersPage } from "./pages/UsersPage";
+import { AppRoutes } from "./pages/AppRoutes";
 
 export const App: React.FunctionComponent = () => {
   const currentUser = useCurrentUser();
@@ -26,33 +14,7 @@ export const App: React.FunctionComponent = () => {
       <AppTopBar />
 
       <main>
-        <Routes>
-          <Route path="/" element={<BooksPage />} />
-          <Route path="/page/:page*" element={<BooksPage />} />
-          <Route path="/books/:id" element={<BookDetailsPage />} />
-          <Route path="/authors" element={<AuthorsPage />} />
-          <Route path="/authors/:id" element={<AuthorDetailsPage />} />
-          <Route path="/users" element={<UsersPage />} />
-          <Route path="/users/:id" element={<UserDetailsPage />} />
-
-          {currentUser ? (
-            <>
-              <Route
-                path="/my/profile"
-                element={<ProfilePage currentUser={currentUser} />}
-              />
-
-              <Route path="/my/books" element={<MyBooksPage />} />
-            </>
-          ) : (
-            <>
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/signup" element={<SignupPage />} />
-            </>
-          )}
-
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <AppRoutes currentUser={currentUser} />
       </main>
 
       <div id="bookshelf-portal" />
---
+++ apps/web/src/pages/AppRoutes.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { CurrentUserFragment } from "../components/AuthContext/CurrentUser.fragment.generated";
+import { AuthorDetailsPage } from "./AuthorDetailsPage";
+import { AuthorsPage } from "./AuthorsPage";
+import { BookDetailsPage } from "./BookDetailsPage";
+import { BooksPage } from "./BooksPage";
+import { LoginPage } from "./LoginPage";
+import { MyBooksPage } from "./MyBooksPage";
+import { NotFoundPage } from "./NotFoundPage";
+import { ProfilePage } from "./ProfilePage";
+import { SignupPage } from "./SignupPage";
+import { UserDetailsPage } from "./UserDetailsPage";
+import { UsersPage } from "./UsersPage";
+
+type Props = {
+  currentUser?: CurrentUserFragment;
+};
+
+export const AppRoutes: React.FunctionComponent<Props> = ({ currentUser }) => (
+  <Routes>
+    <Route path="/" element={<BooksPage />} />
+    <Route path="/page/:page*" element={<BooksPage />} />
+    <Route path="/books/:id" element={<BookDetailsPage />} />
+    <Route path="/authors" element={<AuthorsPage />} />
+    <Route path="/authors/:id" element={<AuthorDetailsPage />} />
+    <Route path="/users" element={<UsersPage />} />
+    <Route path="/users/:id" element={<UserDetailsPage />} />
+
+    {currentUser ? (
+      <>
+        <Route
+          path="/my/profile"
+          element={<ProfilePage currentUser={currentUser} />}
+        />
+
+        <Route path="/my/books" element={<MyBooksPage />} />
+      </>
+    ) : (
+      <>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </>
+    )}
+
+    <Route path="*" element={<NotFoundPage />} />
+  </Routes>
+);
