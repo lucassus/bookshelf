cd6c9fe90b47e55365e7344553e777f197aa606c
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-09-28 23:34:18 +0200

    Remove TODO

--- apps/server/src/graphql/authentication/authenticateContext.ts
+++ apps/server/src/graphql/authentication/authenticateContext.ts
@@ -2,7 +2,6 @@ import { sendAuthCookie } from "../../common/authentication";
 import { User } from "../../database/entity";
 import { Context } from "../context";
 
-// TODO: Find a better name or refactor
 export function authenticateContext(context: Context, user: User): void {
   context.currentUser = user;
   sendAuthCookie(context.res, user);
