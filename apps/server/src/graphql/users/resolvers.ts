8612727b0fb8f84f834fde83935ba94464a4b840
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-10-09 15:26:29 +0200

    Refactor: Update user errors handling (#501)

--- apps/server/src/graphql/users/resolvers.ts
+++ apps/server/src/graphql/users/resolvers.ts
@@ -1,3 +1,4 @@
+import { QueryFailedError } from "typeorm";
 import { EntityNotFoundError } from "typeorm/error/EntityNotFoundError";
 
 import { User } from "../../database/entity";
@@ -81,19 +82,42 @@ const resolvers: Resolvers<Context> = {
       };
     },
 
-    // TODO: Validate uniqueness of email, respond with validation errors
     updateUser: async (rootValue, args, { container }) => {
       const { id, ...userAttributes } = args.input;
 
-      const service = container.get(UsersService);
-      const user = await service.findByIdOrFail(id);
-      const updatedUser = await service.update(user, userAttributes);
+      try {
+        const service = container.get(UsersService);
 
-      return {
-        success: true,
-        message: "User was successfully updated.",
-        user: updatedUser
-      };
+        const user = await service.findByIdOrFail(id);
+        const updatedUser = await service.update(user, userAttributes);
+
+        return updatedUser;
+      } catch (error) {
+        if (error instanceof EntityNotFoundError) {
+          return {
+            __typename: "ResourceNotFoundError",
+            message: "Could not find User"
+          };
+        }
+
+        if (
+          error instanceof QueryFailedError &&
+          error.message ===
+            "SQLITE_CONSTRAINT: UNIQUE constraint failed: users.email"
+        ) {
+          return {
+            __typename: "ValidationErrors",
+            errors: [
+              {
+                path: "email",
+                message: "The given email is already taken!"
+              }
+            ]
+          };
+        }
+
+        throw error;
+      }
     },
 
     deleteUser: async (rootValue, { id }, { container }) => {
