ab8ea61e16e86c5d5d2fc26126042b428cb11730
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-10-09 10:10:20 +0200

    Move resources to a separate module

---
+++ apps/server/src/graphql/resources/resolvers.ts
@@ -0,0 +1,28 @@
+import { GraphQLScalarType } from "graphql";
+
+import { toExternalId, toInternalId } from "../../common/secureId";
+import { findAnythingOrFail } from "../../database/findAnythingOrFail";
+import { Context } from "../context";
+import { Resolvers } from "../resolvers-types.generated";
+
+const resolvers: Resolvers<Context> = {
+  Query: {
+    resource: (rootValue, { id }, { connection }) =>
+      findAnythingOrFail(id, connection),
+
+    anything: (rootValue, { id }, { connection }) =>
+      findAnythingOrFail(id, connection)
+  },
+
+  ExternalID: new GraphQLScalarType({
+    name: "ExternalID",
+    // `serialize` is handled by `Resource.id` resolver, because it requires object type
+    parseValue: (externalId) => toInternalId(externalId)
+  }),
+
+  Resource: {
+    id: (resource) => toExternalId(resource)
+  }
+};
+
+export default resolvers;
