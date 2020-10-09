19817cbb5b336182a09727751378ba48ca84dd68
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-10-09 10:20:52 +0200

    Add simple resources query

--- apps/server/src/graphql/resources/resolvers.ts
+++ apps/server/src/graphql/resources/resolvers.ts
@@ -1,12 +1,20 @@
 import { GraphQLScalarType } from "graphql";
 
 import { toExternalId, toInternalId } from "../../common/secureId";
+import { Author, Book } from "../../database/entity";
 import { findAnythingOrFail } from "../../database/findAnythingOrFail";
 import { Context } from "../context";
 import { Resolvers } from "../resolvers-types.generated";
 
 const resolvers: Resolvers<Context> = {
   Query: {
+    resources: async (rootValue, args, { connection }) => {
+      const authors = await connection.getRepository(Author).find();
+      const books = await connection.getRepository(Book).find();
+
+      return [...authors, ...books];
+    },
+
     resource: (rootValue, { id }, { connection }) =>
       findAnythingOrFail(id, connection),
 
---
+++ apps/server/src/graphql/resources/tests/resources.query.test.ts
@@ -0,0 +1,61 @@
+import { gql } from "apollo-server-express";
+
+import { toExternalId } from "../../../common/secureId";
+import { createTestClient } from "../../../testUtils/createTestClient";
+import { createAuthor, createBook } from "../../../testUtils/factories";
+
+test("resources query", async () => {
+  // Given
+  const author = await createAuthor({
+    name: "Frank Herbert",
+    bio: "American science-fiction writer"
+  });
+
+  const book = await createBook({
+    author,
+    title: "Dune",
+    description:
+      "Dune is a 1965 science-fiction novel by American author Frank Herbert"
+  });
+
+  // When
+  const resp = await createTestClient().query({
+    query: gql`
+      query {
+        resources {
+          __typename
+          id
+
+          ... on Author {
+            name
+            bio
+          }
+
+          ... on Book {
+            title
+            description
+          }
+        }
+      }
+    `
+  });
+
+  // Then
+  expect(resp.errors).toBe(undefined);
+  expect(resp.data).toMatchObject({
+    resources: [
+      {
+        __typename: "Author",
+        id: toExternalId(author),
+        name: author.name,
+        bio: author.bio
+      },
+      {
+        __typename: "Book",
+        id: toExternalId(book),
+        title: book.title,
+        description: book.description
+      }
+    ]
+  });
+});
