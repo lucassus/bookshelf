aa576dc546c201596e9974f88437191c449f1307
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2020-10-25 07:19:24 +0100

    Install clsx package

--- apps/web/src/components/Card/Card.tsx
+++ apps/web/src/components/Card/Card.tsx
@@ -1,12 +1,11 @@
-import classNames from "classnames";
+import clsx from "clsx";
 import React, { HTMLAttributes } from "react";
 
 import styles from "./Card.scss";
 
 type Props = HTMLAttributes<HTMLDivElement>;
 
-// TODO: Use cslx
 export const Card: React.FunctionComponent<Props> = ({
   className,
   ...props
-}) => <div className={classNames(styles.container, className)} {...props} />;
+}) => <div className={clsx(styles.container, className)} {...props} />;
